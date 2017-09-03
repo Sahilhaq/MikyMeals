890b8f712dd8fc3dfb155298908cceae0cfc6453
Sahil <sahilhaque24.10@gmail.com>
2017-09-03 17:51:29 +0530

    Completed styling testimonial section and live-chat box

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,5 +1,7 @@
 import MobileMenu from './modules/_MobileMenu';
 import StickyHeader from './modules/_StickyHeader';
+import LiveChat from './modules/_LiveChat';
 
 var mobileMenu = new MobileMenu();
 var stickyHeader = new StickyHeader();
+var liveChat = new LiveChat();
